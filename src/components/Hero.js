872c40b5cf4a44b34189bc2248dcdcb7e9c3c860
Steven Nagie <sanagie@yahoo.com>
2016-11-23 13:40:31 -0700

    First gh-pages.

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,8 @@
 import React from "react";
 
 // Have to get name, etc from the Select page.
-// My thinking is that we either have to use redux to pass the state of the select page over, or create a function on the select page that selects a certain user, and then import that function. 
+// My thinking is that we either have to use redux to pass the state of the select page over, or create a function on the select page that selects a certain user, and then import that function.
+//Here's another comment.
 
 export default class Hero extends React.Component {
   render() {
